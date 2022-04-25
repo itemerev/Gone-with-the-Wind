0ab729b2288070fca86eb5faea662b98c27bbbfa
Ilya Temerev <kidmoto@yandex.ru>
2022-04-25 23:27:54 +0300

    renamed calc.py to core.py, update core.py and update development_map

---
+++ core.py
@@ -0,0 +1,12 @@
+import datetime
+
+
+class SingleIncome:
+    def __init__(self, category, value):
+        self.date = datetime.date.today()
+        self.category = category
+        self.value = value
+
+
+class DayIncome:
+    pass
