45f418297418daffc5f0fb824fa35f4395f23ff1
Ilya Temerev <kidmoto@yandex.ru>
2022-04-26 22:40:31 +0300

    Update core and add get_and_write module for SQL requests

--- core.py
+++ core.py
@@ -1,11 +1,20 @@
 import datetime
+import get_and_write
 
 
 class SingleIncome:
+    income_id = 0
+
     def __init__(self, category, value):
         self.date = datetime.date.today()
         self.category = category
         self.value = value
+
+        get_and_write.create_day_income()
+
+    def write_data(self):
+        get_and_write.write_single_income(self.income_id, self.date, self.category, self.value)
+        self.income_id += 1
 
 
 class ConstantIncome:
---
+++ get_and_write.py
@@ -0,0 +1,35 @@
+import sqlite3
+
+conn = sqlite3.connect('day_income.db')
+cur = conn.cursor()
+
+
+def create_day_income():
+    cur.execute('''CREATE TABLE IF NOT EXISTS day_income(
+        single_income_id INT PRIMARY KEY,
+        date TEXT,
+        category TEXT,
+        value TEXT);    
+    ''')
+
+
+def create_month_income():
+    cur.execute('''CREATE TABLE IF NOT EXISTS month_income(
+        day_income_id INT PRIMARY KEY,
+        date TEXT,
+        amount_per_day TEXT,
+        budget_for_day TEXT,
+        balance TEXT);    
+    ''')
+
+
+def write_single_income(single_income_id, date, category, value):
+    day_income = (single_income_id, date, category, value)
+    cur.execute("INSERT INTO day_income VALUES(?, ?, ?, ?);", day_income)
+    conn.commit()
+
+
+def write_month_income(day_income_id, date, amount_per_day, budget_for_day, balance):
+    month_income = (day_income_id, date, amount_per_day, budget_for_day, balance)
+    cur.execute("INSERT INTO month_income VALUES(?, ?, ?, ?, ?);", month_income)
+    conn.commit()
