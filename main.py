5bd260cf02621f2e318b223cfc1707dbcb96712e
Ilya Temerev <kidmoto@yandex.ru>
2022-04-27 21:39:31 +0300

    Refactoring main.py

--- main.py
+++ main.py
@@ -1,24 +1,36 @@
 import core
 import get_and_write
 
-while True:
-    data = input()
-    
-    if data == 'exit':
-        break
 
-    else:
-        income_data = data.split()
-        last_income = get_and_write.get_one_income()
+class ConsoleApp:
+    def __init__(self, entry_data):
+        self.income_data = entry_data.split()
+        self.last_income = get_and_write.get_one_income()
+        self.for_table = core.SingleIncome(self.income_data[1], self.income_data[0])
 
-        income = core.SingleIncome(income_data[1], income_data[0])
-        if not last_income:
-            income.income_id = '0'
+    def date_check(self):
+        return str(self.for_table.date) == str(self.last_income[1])
+
+    def write_income(self):
+        if not self.last_income:
+            self.for_table.income_id = '0'
         else:
-            print(last_income[0], income.income_id)
-            income.income_id = str(int(last_income[0]) + 1)
-        if not last_income or str(income.date) == str(last_income[1]):
-            income.write_data()
-            del income
-        print(get_and_write.get_one_income())
+            self.for_table.income_id = str(int(self.last_income[0]) + 1)
+            self.for_table.write_data()
 
+
+def main():
+    while True:
+        entry_data = input()
+        if entry_data == 'exit':
+            break
+        else:
+            app = ConsoleApp(entry_data)
+            if app.date_check():
+                app.write_income()
+                print(app.last_income)
+                print(get_and_write.get_one_income())
+
+
+if __name__ == '__main__':
+    main()
