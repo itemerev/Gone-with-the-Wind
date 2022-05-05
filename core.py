c87f547e6cd3e86ab902c061ead9de94832500ef
Ilya Temerev <kidmoto@yandex.ru>
2022-05-06 00:14:40 +0300

    Full refactoring core.py and working_with_databases.py, but not the end. File main.py must be modified

--- core.py
+++ core.py
@@ -1,5 +1,34 @@
 import datetime
-import working_with_databases
+import working_with_databases as db
+
+
+class ParseInput:
+    """
+    Распознавание введенного текста
+    """
+
+    def __init__(self, input_text):
+        self.input_text = input_text
+
+        self.expenses_data = None
+        self.value = None
+        self.category = None
+
+    def check_command(self):
+        """
+        Проверка поступающего от пользовтеля текста на наличие команд
+        """
+
+        return '/' in self.input_text
+
+    def parse_single_expense(self):
+        """
+        Разделение поступающего от пользовтеля текста на категорию затрат и сумму затрат
+        """
+
+        self.expenses_data = entry_data.split()
+        self.value = self.expenses_data[0]
+        self.category = self.expenses_data[1]
 
 
 class RegularIncome:
@@ -15,14 +44,32 @@ class RegularExpenses:
 
 
 class SingleExpenses:
-    expenses_id = 0
+    expenses_id = '1'
 
     def __init__(self, category, value):
-        working_with_databases.create_day_expenses()
+        db.create_day_expenses()
 
         self.date = datetime.date.today()
         self.category = category
         self.value = value
 
-    def write_data(self):
-        working_with_databases.write_single_expenses(self.expenses_id, self.date, self.category, self.value)
+        self.last_expenses = db.ReadFromTable.get_last_expenses()
+
+    def check_last_expenses(self):
+        """
+        Проверка на наличие предыдущей записи и присвоение id для текущей записи
+        """
+
+        if not self.last_expenses:
+            self.expenses_id = '1'
+        else:
+            self.expenses_id = str(int(self.last_expenses[0]) + 1)
+
+    def write_single_expenses(self):
+        """
+        Сохранение данных о разовых расходах в БД
+        """
+
+        # Добавить условие, если дата внесения записи изменилась
+        db.WriteToTable.write_single_expenses(self.expenses_id, self.date, self.category, self.value)
+        self.expenses_id += 1
--- working_with_databases.py
+++ working_with_databases.py
@@ -1,51 +1,156 @@
 import sqlite3
 
-conn = sqlite3.connect('gone_wind.db')
-cur = conn.cursor()
+
+class Connect:
+    """
+    Подключение к базе данных
+    (используется только как родительский класс)
+    """
+    def __init__(self):
+        self.con = sqlite3.connect('gone_wind.db')
+        self.cur = self.con.cursor()
+
+    def __del__(self):
+        self.con.close()
 
 
-def create_day_expenses():
-    cur.execute('''CREATE TABLE IF NOT EXISTS day_expenses(
-        single_expenses_id INT PRIMARY KEY,
-        date TEXT,
-        category TEXT,
-        value TEXT);    
-    ''')
+class CreateTables(Connect):
+    """
+    Создание таблиц в БД
+    """
+
+    def create_regular_income(self):
+        """
+        Создание таблицы регулярных доходов
+        """
+
+        self.cur.execute('''CREATE TABLE IF NOT EXISTS regular_income(
+            regular_income_id INT PRIMARY KEY,
+            name_income TEXT,
+            value TEXT);
+        ''')
+
+    def create_single_income(self):
+        """
+        Создание таблицы разовых доходов
+        """
+
+        self.cur.execute('''CREATE TABLE IF NOT EXISTS single_income(
+            single_income_id INT PRIMARY KEY,
+            date TEXT,
+            name_income TEXT,
+            value TEXT);
+        ''')
+
+    def create_regular_expenses(self):
+        """
+        Создание таблицы регулярных расходов
+        """
+
+        self.cur.execute('''CREATE TABLE IF NOT EXISTS regular_expenses(
+            regular_expenses_id INT PRIMARY KEY,
+            category TEXT,
+            value TEXT);
+        ''')
+
+    def create_day_expenses(self):
+        """
+        Создание таблицы расходов за день
+        """
+
+        self.cur.execute('''CREATE TABLE IF NOT EXISTS day_expenses(
+            single_expenses_id INT PRIMARY KEY,
+            date TEXT,
+            category TEXT,
+            value TEXT);    
+        ''')
+
+    def create_month_expenses(self):
+        """
+        Создание таблицы расходов за месяц
+        """
+
+        self.cur.execute('''CREATE TABLE IF NOT EXISTS month_expenses(
+            day_expenses_id INT PRIMARY KEY,
+            date TEXT,
+            amount_per_day TEXT,
+            budget_for_day TEXT,
+            balance TEXT);    
+        ''')
 
 
-def create_month_expenses():
-    cur.execute('''CREATE TABLE IF NOT EXISTS month_expenses(
-        day_expenses_id INT PRIMARY KEY,
-        date TEXT,
-        amount_per_day TEXT,
-        budget_for_day TEXT,
-        balance TEXT);    
-    ''')
+class WriteToTable(Connect):
+    """
+    Запись данных в БД
+    """
+
+    def write_single_expenses(self, single_expenses_id, date, category, value):
+        """
+        Запись разовых расходов в БД в таблицу расходов за день
+        """
+
+        day_expenses = (single_expenses_id, date, category, value)
+        self.cur.execute("INSERT INTO day_expenses VALUES(?, ?, ?, ?);", day_expenses)
+        self.con.commit()
+
+    # Необходимо переименовать атрибут 'amount_per_day'
+    def write_month_expenses(self, day_expenses_id, date, amount_per_day, budget_for_day, balance):
+        """
+        Запись суммы днеынх расходов в БД в таблицу расходов за месяц
+        """
+
+        # Необходимо переименовать атрибут 'amount_per_day'
+        month_expenses = (day_expenses_id, date, amount_per_day, budget_for_day, balance)
+        self.cur.execute("INSERT INTO month_expenses VALUES(?, ?, ?, ?, ?);", month_expenses)
+        self.con.commit()
 
 
-def write_single_expenses(single_expenses_id, date, category, value):
-    day_expenses = (single_expenses_id, date, category, value)
-    cur.execute("INSERT INTO day_expenses VALUES(?, ?, ?, ?);", day_expenses)
-    conn.commit()
+class ReadFromTable(Connect):
+    """
+    Чтение данных из БД
+    """
+
+    def __init__(self):
+        super().__init__()
+
+        self.last_expenses = None
+
+        self.all_line = None
+        self.log_date = None
+
+    def get_last_expenses(self):
+        """
+        Чтение последней записи разовых расходов
+        """
+
+        cur.execute('SELECT * FROM day_expenses ORDER BY single_expenses_id DESC LIMIT 1;')
+        self.last_expenses = cur.fetchone()
+
+    def read_day_expenses(self):
+        """
+        Чтение всех расходов за день
+        """
+
+        self.cur.execute('SELECT * FROM day_expenses')
+        self.all_line = cur.fetchall()
+        self.log_date = all_line[0][1]
 
 
-def write_month_expenses(day_expenses_id, date, amount_per_day, budget_for_day, balance):
-    month_expenses = (day_expenses_id, date, amount_per_day, budget_for_day, balance)
-    cur.execute("INSERT INTO month_expenses VALUES(?, ?, ?, ?, ?);", month_expenses)
-    conn.commit()
+class CreateLog:
+    """
+    Логирование в текстовый файл
+    """
 
+    def __init__(self):
+        self.day_expenses = ReadFromTable()
 
-def get_last_line():
-    cur.execute('SELECT * FROM day_expenses ORDER BY single_expenses_id DESC LIMIT 1;')
-    last_line = cur.fetchone()
-    return last_line
+    def write_day_log(self):
+        """
+        Логирование всех расходов за день в файл '{дата}.txt'
+        """
 
-
-def write_day_log():
-    cur.execute('SELECT * FROM day_expenses')
-    all_line = cur.fetchall()
-    log_date = all_line[0][1]
-    with open(f'Logs/{log_date}.txt', 'w', encoding='utf-8') as log_file:
-        for line in all_line:
-            log_file.write(str(line) + '\n')
+        self.day_expenses.read_day_expenses()
+        with open(f'Logs/{self.day_expenses.log_date}.txt', 'w', encoding='utf-8') as log_file:
+            for line in self.day_expenses.all_line:
+                log_file.write(str(line) + '\n')
 
